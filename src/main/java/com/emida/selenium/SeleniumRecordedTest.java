30d007813ec1e8e2f6c738c19325519b833b85b9
Kevin Rodriguez <krodriguez@emida.net>
2018-01-12 18:58:09 -0500

    Add List to Array Option

--- src/main/java/com/emida/selenium/SeleniumRecordedTest.java
+++ src/main/java/com/emida/selenium/SeleniumRecordedTest.java
@@ -6,6 +6,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import com.querydsl.core.Tuple;
+import java.util.ArrayList;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -18,26 +19,28 @@ public class SeleniumRecordedTest {
 
     WebDriver driver = null;
 
-    public void ReadRecordeds(List<Tuple> listRecorded, String folderImage, String folderSelenum) {
+    public List ReadRecordeds(List<Tuple> listRecorded, String folderImage, String folderSelenum) {
 
         System.setProperty("webdriver.chrome.driver", folderSelenum);
         driver = new ChromeDriver();
         driver.manage().window().maximize();
+        List listOptions = new ArrayList();
         try {
-            for (Tuple item : listRecorded) { 
+            for (Tuple item : listRecorded) {
                 Object oXpath = item.toArray()[0];
                 Object oOption = item.toArray()[1];
                 Object oValue = item.toArray()[2];
-                String xpath = String.valueOf(oXpath);                
+                String xpath = String.valueOf(oXpath);
                 String option = String.valueOf(oOption);
                 String value = String.valueOf(oValue);
-                captureEvent(option, value, xpath, folderImage);
+                captureEvent(option, value, xpath, folderImage);  
             }
             captureEvent("6", "", "", "");
-        } catch (Exception ecv) {
+        } catch (Exception ex) {
             driver.quit();
-            System.out.println(ecv);
+            LOG.log(Level.INFO, "Error{0}", ex);
         }
+        return listOptions;
     }
 
     public void captureEvent(String event, String target, String param, String folderImage) {
